176bcd62c433042a0406fad6aa749f14d33dee0f
Victor Goff <keeperotphones@gmail.com>
2014-09-03 13:45:32 -0400

    iterating through a collection of seconds

--- 1_week/1c_year_months.rb
+++ 1_week/1c_year_months.rb
@@ -1,18 +1,18 @@
-
 def convert_seconds_to_years_and_months(age_in_seconds)
-
   years_old = age_in_seconds / (60 * 60 * 24 * 365)
-
   remaining_months = (age_in_seconds % (60 * 60 * 24 * 365) ) / ( 60 * 60 * 24 * 30 )
-
-
-
   puts "Being #{age_in_seconds} seconds old is equivalent to being #{years_old} years and #{remaining_months} months old."
-
 end
 
+ages_in_seconds = [
+  979000000,
+  2158493738,
+  246144023,
+  1270166272,
+  1025600095,
+]
 
 
-
-
-convert_seconds_to_years_and_months(2158493738)     # 68 years and 5 months
+ages_in_seconds.each do |seconds|
+  convert_seconds_to_years_and_months(seconds)     # 68 years and 5 months
+end
