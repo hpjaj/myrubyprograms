3d00c0d8f843eb7866b9c1d0f7180edc2e2ed02d
hpjaj <hmlevine@gmail.com>
2014-09-15 15:35:00 -0600

    Deaf Granny is starting to come together

--- 3_week/6e_deaf_granny_part_1.rb
+++ 3_week/6e_deaf_granny_part_1.rb
@@ -1,17 +1,21 @@
+# We won't test this at the moment, too high level  (Acceptance test level)
 def start_program
   puts "Soooo, what's new, sonny boy?"
   gets_grandsons_input
 end
 
-def gets_grandsons_input
+def gets_grandsons_input #  Goto statements are not great, they can make for spaghetti code
   print "> "
-  @grandson_choice = gets.chomp 
+  @grandson_choice = gets.chomp
   determines_if_grandma_heard
 end
 
-def determines_if_grandma_heard
+# doctest: grandma_heard?
+# >> determines_if_grandma_heard("SHOUTED")
+# => true
+def determines_if_grandma_heard # Too complicated, doesn't return, but actually routes business logic we won't test for right now
   if @grandson_choice == 'BYE'
-    exit_program
+    grannys_response_to_bye
   elsif @grandson_choice == @grandson_choice.upcase
     calculates_grandmas_random_year_response
   else
@@ -19,9 +23,11 @@ def determines_if_grandma_heard
   end
 end
 
-def exit_program
-  puts "Goodbye sonny.  Come back soon."
-  exit(0)
+# doctest: Granny always responds to "BYE"
+# >> grannys_response_to_bye
+# => "Goodbye Sonny.  Come back soon."
+def grannys_response_to_bye
+  "Goodbye Sonny.  Come back soon."
 end
 
 def calculates_grandmas_random_year_response
@@ -41,4 +47,24 @@ def grandma_cannot_hear_you
   gets_grandsons_input
 end
 
-start_program
+# ====================
+# doctest: grannys_greeting
+# >> grannys_greeting('Sonny')
+# => "Hello Sonny!"
+def grannys_greeting(name)
+  "Hello #{name}!"
+end
+if __FILE__ == $0 # This is an "application guard" or also called a "Library guard"
+  # Code I wish I had.   Wish driven development.
+  puts "This discussion will continue until you, as our feeble hero 'Sonny', shouts 'BYE'."
+  puts
+  puts grannys_greeting('Sonny')
+  exit
+  sonny_says = prompt_sonny
+  until sonny_says == 'BYE'
+    granny_responds_to(sonny_says)
+    sonny_says = prompt_sonny
+  end
+
+end
+
--- pair_programming/hello.rb
+++ pair_programming/hello.rb
@@ -1,3 +1,25 @@
 def hello(name = 'World', punctuation = '!')
-  "Hello #{name}#{punctuation}"
+  "Hello#{',' if punctuation == '?'} #{name}#{punctuation}"
 end
+
+puts hello
+puts hello "Mickey"
+# I really want this to display "Hello, Minnie?"
+puts hello('Minnie', '?')
+
+__END__
+1st run
+Hello World!
+Hello Mickey!
+Hello Minnie?
+
+
+Hello, World!
+Hello, Mickey!
+Hello, Minnie?
+
+3rd run
+
+Hello World!
+Hello Mickey!
+Hello, Minnie?
