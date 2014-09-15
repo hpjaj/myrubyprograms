080c26e355aed3d716d03cca71da1e2cce6cd4be
Victor Goff <keeperotphones@gmail.com>
2014-09-15 00:38:26 -0400

    Getting ready for more testing

--- pair_programming/hello.rb
+++ pair_programming/hello.rb
@@ -1,16 +1,3 @@
-=begin
-doctest: I can greet the world
->> hello
-=> "Hello World!"
-doctest: I can greet someone personally
->> hello('Harry')
-=> "Hello Harry!"
-doctest: I can ask if someone is there
->> hello('Vic', '?')
-=> "Hello Vic?"
-=end
-
-def hello(name='World', punctuation='!')
+def hello(name = 'World', punctuation = '!')
   "Hello #{name}#{punctuation}"
 end
-
