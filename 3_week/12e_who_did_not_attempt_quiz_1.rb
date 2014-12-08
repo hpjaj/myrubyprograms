d3e984753e28dc71603aa29dfff9946943ce0d09
hpjaj <hmlevine@gmail.com>
2014-12-08 16:09:14 -0700

    Used the append operator to solve the challenge

--- 3_week/12e_who_did_not_attempt_quiz_1.rb
+++ 3_week/12e_who_did_not_attempt_quiz_1.rb
@@ -1,6 +1,10 @@
-quiz = [0,0,0,1,0,0,1,1,0,1]
-non_attempts_total = 0
-quiz.each do |num|
-  (non_attempts_total = non_attempts_total += 1) if num == 0
+def quiz_completion_numbers(arr)
+  incomplete = []
+  arr.each do |num|
+    ( incomplete << num ) if ( num == 0 )
+  end
+  "The number of participants who did not attempt Quiz 1 is #{incomplete.length} out of #{arr.length} total participants."
 end
-puts "The number of participants who did not attempt Quiz 1 is #{non_attempts_total} out of #{quiz.length} total participants."
+
+quiz = [0,0,0,1,0,0,1,1,0,1]
+puts quiz_completion_numbers(quiz)
