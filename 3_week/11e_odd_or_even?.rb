2fc39993cc56f09756be153622fc900f6f150346
hpjaj <hmlevine@gmail.com>
2014-12-08 15:43:11 -0700

    Used the ternary operator in the odd_or_even method.

--- 3_week/11e_odd_or_even?.rb
+++ 3_week/11e_odd_or_even?.rb
@@ -1,12 +1,9 @@
-# Exercise11. Write a Ruby program that, when given an array: 
-# collection = [12, 23, 456, 123, 4579] 
-# prints each number, 
-# and tells you whether it is odd or even.
-
-collection = [12, 23, 456, 123, 4579] 
-
-collection.each do |num|
-  print num
-  puts num % 2 == 0 ? " is an even number." : " is an odd number."
+def odd_or_even(arr)
+  arr.each do |num|
+    print num
+    puts num % 2 == 0 ? ' is even' : ' is odd'
+  end
 end
 
+collection = [12, 23, 456, 123, 4579]
+odd_or_even(collection)
