13cdfd26f0554c55521dcb0254733cdf70948e43
hpjaj <hmlevine@gmail.com>
2014-12-16 12:12:31 -0700

    Added the concept of super into both files.  Reorderded the content of each method to work correctely with super.

--- 5_week/2e_shapes.rb
+++ 5_week/2e_shapes.rb
@@ -1,32 +1,34 @@
 class Shape
   def rotate_on_click
-    #shape rotates 360 degrees on click
     #using CSS and/or jQuery
+    #shape rotates 360 degrees on click
+    #pivot point is the center of the shape
   end
 
   def music_on_click
-    #shape plays song A on click
     #using CSS and/or jQuery
+    #sound file is type .AIF
+    #shape plays song A on click
   end
 end
 
 class Square < Shape
   def music_on_click
+    #super
     #shape plays song B on click
-    #using CSS and/or jQuery
   end
 end
 
 class Circle < Shape
   def music_on_click
+    #super
     #shape plays song C on click
-    #using CSS and/or jQuery
   end
 end
 
 class Triange < Shape
   def music_on_click
+    #super
     #shape plays song D on click
-    #using CSS and/or jQuery
   end
 end
---
+++ 5_week/2e_spec_change.rb
@@ -0,0 +1,16 @@
+require './2e_shapes'
+
+class Amoeba < Shapes
+
+  def rotate_on_click
+    #super
+    #pivot point is on the end of the Amoeba
+  end
+
+  def music_on_click
+    #super
+    #sound file is type .hif
+    #shape plays song E on click
+  end
+  
+end
