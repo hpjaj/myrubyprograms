35f9d9ca9a0fa790a0b23ea125c99e1a9bdd1665
hpjaj <hmlevine@gmail.com>
2014-12-16 11:24:47 -0700

    Removed commented code

--- 5_week/unpredictablestring.rb
+++ 5_week/unpredictablestring.rb
@@ -11,14 +11,6 @@ class UnpredictableString < String
 
   def scramble
     scrambled = @stringer.split("").shuffle.join
-  #   arr = stringer.split("")
-  #   scrambled_arr = []
-  #   until arr.length == 0
-  #     rand_position = rand(arr.length)
-  #     scrambled_arr << arr[rand_position]
-  #     arr.delete_at(rand_position)  
-  #   end
-  #   scrambled_arr.join
   end
 
 end
