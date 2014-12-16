bc4fb82114fbec71c69859a98dbcc83837a453ca
hpjaj <hmlevine@gmail.com>
2014-12-16 11:57:50 -0700

    First swing at the second exercise, fifth week.  Spec requires CSS and JavaScript for completion.

---
+++ 5_week/2e_shapes.rb
@@ -0,0 +1,32 @@
+class Shape
+  def rotate_on_click
+    #shape rotates 360 degrees on click
+    #using CSS and/or jQuery
+  end
+
+  def music_on_click
+    #shape plays song A on click
+    #using CSS and/or jQuery
+  end
+end
+
+class Square < Shape
+  def music_on_click
+    #shape plays song B on click
+    #using CSS and/or jQuery
+  end
+end
+
+class Circle < Shape
+  def music_on_click
+    #shape plays song C on click
+    #using CSS and/or jQuery
+  end
+end
+
+class Triange < Shape
+  def music_on_click
+    #shape plays song D on click
+    #using CSS and/or jQuery
+  end
+end
